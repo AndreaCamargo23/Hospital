003b919c9d4f39ee27bd1e8528154ba2779faf69
AndreaCamargo23 <camargomilena26@gmail.com>
2021-10-06 09:54:54 -0500

    primer proyecto

---
+++ baseDatos/conexionbd.php
@@ -0,0 +1,21 @@
+<?php
+class Conexion{
+    public static function Conectar(){
+        //Definición de variables 
+        //Primer campo: Nombre de la variable
+        //Segundo campo: valor de la variable
+        define('servidor','localhost');
+        define('usuario','root');
+        define('password','');
+        define('bdname','bd_hospital');
+        $op=array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8');//pARA LAS TILDES, COMAS DE LA BD
+        try{
+            //Realizar la conexion a la bd
+            $link = new PDO("mysql:host=".servidor.";port=3308;dbname=".bdname,usuario,password,$op);
+            return $link;
+        }catch(Exception $ex){//Por si se presenta alguna excepción 
+            die("Error al conectar en la base de datos ".$ex->getMessage()); 
+        }
+    }
+}
+?>
---
+++ baseDatos/crudPaciente.php
@@ -0,0 +1,89 @@
+<?php
+$contador =+1;
+include './conexionbd.php'; 
+//Creación del objeto de la clase 
+$obBD = new Conexion(); 
+$link = $obBD->Conectar(); //crear conexion 
+//datos de variables del formulario
+$id=(isset($_POST['id']))?$_POST['id']:''; 
+$nom=(isset($_POST['nom']))?$_POST['nom']:'';
+$ape=(isset($_POST['ape']))?$_POST['ape']:'';
+$dire=(isset($_POST['dire']))?$_POST['dire']:'';
+$fecha_nac=(isset($_POST['fecha_nac']))?$_POST['fecha_nac']:'';
+$email=(isset($_POST['email']))?$_POST['email']:'';
+$genero=(isset($_POST['genero']))?$_POST['genero']:'';
+$rh=(isset($_POST['rh']))?$_POST['rh']:'';
+$estado=(isset($_POST['estado']))?$_POST['estado']:'';
+$celu=(isset($_POST['celu']))?$_POST['celu']:'';
+$opciones=(isset($_POST['opcion']))?$_POST['opcion']:''; 
+
+switch($opciones){
+    case 1://Insertar datos
+	try{
+		$sql="INSERT INTO paciente (id_paciente,nombre,apellido,direccion,fecha_nac,email,celular,id_genero_fk,id_rh_fk,id_estado_fk)
+		VALUES ('$id','$nom','$ape','$dire','$fecha_nac','$email','$celu','$genero','$rh','$estado')";
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        //Mostrar los datos insertado en la tabla
+        $sql ="select * from paciente order by id_paciente desc limit 1"; 
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        $data = $res->fetchAll(PDO::FETCH_ASSOC);//Va almacenar commo un vector
+	} catch(Exception $ex){
+		$data = null; 
+	}
+    break; 
+    
+    case 2://Actualizar datos 
+	try{
+		$sql ="UPDATE paciente SET nombre='$nom',apellido='$ape',direccion='$dire',fecha_nac='$fecha_nac',email='$email',id_genero_fk='$genero',id_rh_fk='$rh',
+		id_estado_fk='$estado' WHERE id_paciente=$id";
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        //Mostrar los datos insertdos en la tabla de Alumnos
+        $sql ="select * from paciente order by id_paciente desc limit 1"; 
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        $data = $res->fetchAll(PDO::FETCH_ASSOC);
+	} catch(Exception $ex){
+		$data = null; 
+	}        
+        break; 
+    
+
+    case 3://Cambiar estado a inactivo 
+      try{
+		$sql ="update paciente set id_estado_fk='7' where id_paciente=$id"; 
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        $sql ="select * from empleado where id_empleado=$id"; 
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        $data = $res->fetchAll(PDO::FETCH_ASSOC);
+	} catch(Exception $ex){
+		$data = null; 
+	}
+        break; 
+    
+
+    case 4://Consultar
+        $sql ="SELECT id_paciente, concat(nombre, apellido), direccion, fecha_nac, email, celular,TIMESTAMPDIFF(YEAR,fecha_nac,CURDATE()) AS edad, genero.genero,
+		rh.rh, estado.estado FROM paciente join genero on(genero.id_genero=paciente.id_genero_fk) 
+		join rh on (rh.id_rh=paciente.id_rh_fk) join estado on (estado.id_estado=paciente.id_estado_fk)"; 
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        $data = $res->fetchAll(PDO::FETCH_ASSOC);//Va almacenar commo un vector        
+        break; 
+		
+    case 5://Consultar solo los datos de un solo usuario 
+        $sql ="select * from paciente where id_paciente=$id";
+        $res = $link->prepare($sql);//Prepara la consulta para su ejecución
+        $res->execute(); //Ejecuta la consulta
+        $data = $res->fetchAll(PDO::FETCH_ASSOC);//Va almacenar commo un vector
+        break;
+    
+}
+print json_encode($data,JSON_UNESCAPED_UNICODE); 
+//Envio el vector en formato json a AJAX 
+$link = null; 
+?>
